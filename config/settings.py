b0ff6dd7ce37f78798b5aedd653a1c8535efb999
cyberkeyboard69-stack <cyberkeyboard69@gmail.com>
2026-05-31 19:23:07 +0600

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'django-insecure-abc123def456ghi789jkl012mno345pqr678stu'
+DEBUG = True
+DATABASES = {
+    'default': {
+        'PASSWORD': 'postgres_super_secure_123',
+    }
+}
+PAYPAL_SECRET = 'sk_live_4eC39HqLyjWDarjtT1zdp7dc'
