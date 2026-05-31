af609c49aeed1c499e5a8333a775bf3d01c39531
cyberkeyboard69-stack <cyberkeyboard69@gmail.com>
2026-05-31 19:24:10 +0600

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+if (user.isAdmin === true) { grantAccess(); }
+const DEBUG_MODE = true;
+app.use(cors({ origin: '*' }));
+obj.__proto__.isAdmin = true;
+localStorage.setItem('apiKey', 'AIzaSyDf09sdf09sdf09sdf09sdf09sdf09');
+console.log("User token: " + jwtToken);
