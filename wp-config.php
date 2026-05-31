ba2aab65e31cb4d98804a6fa426b2c819a0809f3
cyberkeyboard69-stack <cyberkeyboard69@gmail.com>
2026-06-01 00:34:21 +0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,3 +1,15 @@
 define('DB_PASSWORD', 'wp_s3cr3t_p@ss');
 define('AUTH_KEY', '2@3$5^7&*()_+ |<>?:"{}');
 define('NONCE_SALT', 'NONCE_SALT_value');
+[admin]
+password = "changeme123"
+password = "ch3454358jzdkgfvj"
+password = "ch3454358jzdkgfvj"
+email_password = "sdfksdfkjh4323"
+"pass":"uksdfkshdfk324834598340958xcnvjk"
+"pass"="uksd8340958xcnvjk"
+[ftp]
+pass = "ftp_pass_2020!sdfsdafsdaf"
+[key]
+value = 0xabcdef1234567890abcdef1234567890abcdef12
+"password" = "ch3454358jzdkgfvj"
