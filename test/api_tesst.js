9cbe9f88e94a8b935dc89204cd9b08ae892c8301
cyberkeyboard69-stack <cyberkeyboard69@gmail.com>
2026-05-31 19:27:35 +0600

    Create api_tesst.js

---
+++ test/api_tesst.js
@@ -0,0 +1,3 @@
+const authHeader = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.xyz';
+window.location.href = req.query.redirectUrl;
+const fakeSecret = "d41d8cd98f00b204e9800998ecf8427e";
